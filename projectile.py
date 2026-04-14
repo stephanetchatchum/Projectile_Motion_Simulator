76d983a84fbf3f1867a5b8ac30cfe1f565788622
stephanetchatchum@gmail.com <stephanetchatchum@gmail.com>
2026-04-14 23:50:50 +0200

    skeleton

---
+++ projectile.py
@@ -0,0 +1,17 @@
+import math
+import matplotlib.pyplot as plt
+
+# PART 1: Input (what the user controls)
+
+# PART 2: Convert angle to radians
+
+# PART 3: Calculate initial velocity components
+
+# PART 4: Simulate (calculate all positions until ball hits ground)
+# TODO: Loop and collect positions
+
+# PART 5: Plot the trajectory
+# TODO: Draw the path
+
+# PART 6: Display stats
+# TODO: Show numbers
