2711e7ee1ad90bf6c8688d1417783ce5b8b13138
stephanetchatchum@gmail.com <stephanetchatchum@gmail.com>
2026-04-14 23:53:33 +0200

    Basic info

--- projectile.py
+++ projectile.py
@@ -2,10 +2,16 @@ import math
 import matplotlib.pyplot as plt
 
 # PART 1: Input (what the user controls)
+angle_deg = 45
+speed = 20
+gravity = 9.8
 
 # PART 2: Convert angle to radians
+angle_rad = math.radians(angle_deg)
 
 # PART 3: Calculate initial velocity components
+Ux = speed * math.cos(angle_rad)
+Uy = speed * math.sin(angle_rad)
 
 # PART 4: Simulate (calculate all positions until ball hits ground)
 # TODO: Loop and collect positions
